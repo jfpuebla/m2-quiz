4d4a5109562bdc817ff7e23bff4870ac18d2a2a8
jfpuebla <johnfrancispuebla21@gmail.com>
2022-01-07 21:14:30 +0800

    modifying js

--- js-track.js
+++ js-track.js
@@ -1,16 +1,3 @@
-
-
-// const weightValue=document.getElementById('weight-input').value;
-// const heightValue=document.getElementById('height-input').value;
-
-// const genderValue=document.querySelector('input[name="radio-button"]:checked');
-// if (genderValue.value=='Male'){
-//     console.log(genderValue.value);
-//     }else if(genderValue.value=='Female') {
-//         console.log(genderValue.value)
-//     }
-
-
   
 const submitBtn=document.getElementById('submit-btn');
 
@@ -19,13 +6,47 @@ submitBtn.onclick=function (){
     var weightValue=document.getElementById('weight-input').value;
     var heightValue=document.getElementById('height-input').value;
     var genderValue=document.querySelector('input[name="radio-button"]:checked').value;
-
     var activity = document.getElementById("list");
     var activityValue = activity.options[activity.selectedIndex].value;
 
-    console.log(ageValue);
-    console.log(weightValue);
-    console.log(heightValue);
-    console.log(genderValue);
-    console.log(activityValue);
+    var bmr=findBMR(ageValue,weightValue,heightValue,genderValue);
+    console.log(bmr);
+
+    //uses the activity method
+    var finalBMR=findActivityBMR(bmr);
+    console.log(finalBMR);
+
+
 }
+//finds the bmr
+function findBMR(ageValue,weightValue,heightValue,genderValue){
+    var bmr;
+
+    if(genderValue=='Male'){
+        bmr=(13.397*weightValue) + (4.799*heightValue)-(5.5677*ageValue)+88.362;
+    }else if (genderValue=='Female'){
+        bmr=(9.247*weightValue) + (3.098*heightValue)-(4.330*ageValue)+447.593;
+    }
+    return bmr;
+}
+
+//activity multiplies the bmr
+function findActivityBMR(bmr){
+    var finalBMR;
+
+    if(activityValue=='1'){
+        finalBMR=1.2*bmr;
+    }else if(activityValue=='2'){
+        finalBMR=1.375*bmr;
+    }else if(activityValue=='3'){
+        finalBMR=1.55*bmr;
+    }else if(activityValue=='4'){
+        finalBMR=1.725*bmr;
+    }else if(activityValue=='5'){
+        finalBMR=1.9*bmr;
+    }
+    return finalBMR;
+}
+
+var calorieTotal=document.getElementById('calorie-left');
+calorieTotal.innerHTML=finalBMR;
